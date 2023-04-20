2692079547a505443587b5404975f3e0e0594ed1
Shreyansh <shreyanshop20+2@outlook.com>
2023-04-20 20:43:06 +0530

    Create SQLiteTask.php

---
+++ src/Shreyansh/FastTP/tasks/SQLiteTask.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shreyansh\FastTP\tasks;
+
+use pocketmine\scheduler\AsyncTask;
+use SQLite3;
+
+class SQLiteTask extends AsyncTask {
+
+    private string $query;
+
+    public function __construct(string $query) {
+        $this->query = $query;
+    }
+
+    public function onRun() {
+        $db = new SQLite3($this->getDataFolder() . "FastTP.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
+        $db->exec($this->query);
+        $db->close();
+    }
+}
