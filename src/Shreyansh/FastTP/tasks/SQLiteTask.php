0153d58108713a0c7c30f67c378126d5f0e6a753
Shreyansh <shreyanshop20+2@outlook.com>
2023-04-20 20:55:35 +0530

    Update SQLiteTask.php

--- src/Shreyansh/FastTP/tasks/SQLiteTask.php
+++ src/Shreyansh/FastTP/tasks/SQLiteTask.php
@@ -15,7 +15,7 @@ class SQLiteTask extends AsyncTask {
         $this->query = $query;
     }
 
-    public function onRun() {
+    public function onRun(): void {
         $db = new SQLite3($this->getDataFolder() . "FastTP.db", SQLITE3_OPEN_READWRITE | SQLITE3_OPEN_CREATE);
         $db->exec($this->query);
         $db->close();
